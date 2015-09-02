bfdc511f67b6dd1f22155470f93beae7c3e48b1e
Vivian Liang <vliang88@gmail.com>
2015-09-02 12:36:22 -0700

    Initial

---
+++ QueerSpaces/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for QueerSpaces project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
+"""
+
+from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
+
+import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "QueerSpaces.settings")
+
+application = Cling(get_wsgi_application())
