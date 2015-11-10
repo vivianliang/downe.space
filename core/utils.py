2e8eb6592cd9bff670fb883d3300498f3e6b7b5c
Mat Geist <matgeist@gmail.com>
2015-11-10 05:47:34 -0800

    use JsonResponse

--- core/utils.py
+++
@@ -1,6 +0,0 @@
-from django.http import HttpResponse
-from simplejson import dumps
-
-
-def render_json(data, status=200):
-  return HttpResponse(dumps(data), content_type='application/json', status=status)
