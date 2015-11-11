1e3df6f2f743b90bb6d10847c8a5972ba0117b42
Mat Geist <matgeist@gmail.com>
2015-11-10 19:46:29 -0800

    rename backend

--- DowneSpace/urls.py
+++ DowneSpace/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'QueerSpaces.views.home', name='home'),
+    # url(r'^$', 'DowneSpace.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^api/', include('core.urls')),
