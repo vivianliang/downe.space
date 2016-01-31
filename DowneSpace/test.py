c964b1f385508703e509fad81afcea0ecc9a76ef
Mat Geist <matgeist@gmail.com>
2016-01-30 19:37:49 -0800

    order by date and exclude past events

--- DowneSpace/test.py
+++ DowneSpace/test.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 from django.contrib.auth.models import User
 from django.test import TestCase as BaseTestCase
 from django.utils import timezone
@@ -31,12 +33,13 @@ class TestCase(BaseTestCase):
     user.save()
     return user
 
-  def create_event(self, name='', description='', lat=None, lon=None, user=None):
-    with patch('core.receivers.get_coords') as get_coords:
-      get_coords.return_value = {'lat': lat or 1.0, 'lng': lon or 1.0}
-      return Event.objects.create(
-        name        = name,
-        description = description,
-        start       = self.now,
-        end         = self.now,
-        contact     = user or self.user)
+  def create_event(self, name='', description='', start=None, end=None,
+    lat=None, lon=None, user=None):
+      with patch('core.receivers.get_coords') as get_coords:
+        get_coords.return_value = {'lat': lat or 1.0, 'lng': lon or 1.0}
+        return Event.objects.create(
+          name        = name,
+          description = description,
+          start       = start or self.now,
+          end         = end or self.now + timedelta(days=1),
+          contact     = user or self.user)
